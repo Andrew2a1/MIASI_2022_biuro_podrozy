929395c27fa631396b89e5b1685d24e2752b00e6
Cezary Bujak <cezarybujak28@gmail.com>
2022-05-17 21:23:16 +0200

    Tests for nothing

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
-docker run -p 8080:8080 -d --name flowable -v %cd%\flowable-data:/data -v %cd%\flowable-db:/opt/tomcat/flowable-db -e FLOWABLE_CONTENT_STORAGE_ROOT-FOLDER=/data flowable/all-in-one 2> nul || docker start flowable
+docker run -p 8080:8080 -d --name flowable -v "%cd%\flowable-data":/data -v "%cd%\flowable-db":/opt/tomcat/flowable-db -e FLOWABLE_CONTENT_STORAGE_ROOT-FOLDER=/data flowable/all-in-one 2> nul || docker start flowable
 docker logs --follow flowable
